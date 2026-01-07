0047dd4143976ebd407fc711d6c0f2631c9c9b39
Nedajist <Kaimeng44@proton.me>
2026-01-06 18:31:02 -0800

    completed minigame 1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float _speed = 1.0f;
+    [SerializeField] private float _speed = 1f;
     [SerializeField] private Transform _playerTransform;
     [SerializeField] private GameObject _plantPrefab;
     [SerializeField] private int _numSeeds = 5; 
@@ -13,16 +13,49 @@ public class Player : MonoBehaviour
 
     private void Start ()
     {
-        
+        _numSeedsLeft = _numSeeds;
+        _numSeedsPlanted = 0;
+        _plantCountUI.UpdateSeeds(_numSeedsPlanted, _numSeedsLeft);
     }
 
     private void Update()
     {
-        
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            _playerTransform.Translate(Vector2.left * _speed * 0.005f);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            _playerTransform.Translate(Vector2.right * _speed * 0.005f);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            _playerTransform.Translate(Vector2.up * _speed * 0.005f);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            _playerTransform.Translate(Vector2.down * _speed * 0.005f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlantSeed();
+        }
     }
 
     public void PlantSeed ()
     {
-        
+        if (_numSeedsLeft > 0)
+        {
+            _numSeedsLeft -= 1;
+            _numSeedsPlanted += 1;
+            _plantCountUI.UpdateSeeds(_numSeedsPlanted, _numSeedsLeft);
+
+            GameObject _plant = Instantiate(_plantPrefab);
+            _plant.transform.position = _playerTransform.position;
+        }
     }
 }
--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Start working on an assignment on the day that it is assigned. This reduces the 
 3. These lines of code call the PrintMessage function, which prints "hello world" to Unity's console. This happens every frame.
 4. MonoBehavior
 5. These lines of code prints "x = 10" to Unity's console when the object with this script first appears in a scene
-6. The 10 is the argument given to PrintMessage(), and the argument is passed to x. "x = " + x is the argument passed to Debug.log. 
+6. The 10 is the argument given to PrintMessage(), and the argument is passed to x. x is the parameter passed to Debug.log. 
 7. Transform is not an object, but a class in Unity. While Translate is a method of the transform class, it can only be called on an instance of the class, not on the class itself. 
 8. Replace Transform with _playerTransform
 
@@ -18,7 +18,10 @@ Start working on an assignment on the day that it is assigned. This reduces the 
 [MG1 breakdown Google doc](https://docs.google.com/document/d/1UQTlTMUv7OoSRXdfaYn3iWpCgYABL-kNvOKmFYxCaUk/edit?tab=t.0)
 
 ## Devlog
-
+1. Kai Meng, he/him
+2. My Minigame breakdown document states that one of the player class's methods will instantiate a seed object. I also believed that the player class has a method which updates the UI text object.
+These plans turned out to be true, as the Player class has a method called PlantSeed() which does both. 
+I thought both the player and seed objects would have a sprite component, and the Player and seed objects do indeed have a sprite renderer component. 
 
 ## Open-Source Assets
 If you added any other outside assets, list them here!
